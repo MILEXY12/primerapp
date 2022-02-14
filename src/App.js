d94dde364f6d348bda7bf18bc72bf12a8b330ade
Milexy zambrano <milexyzambrano.a@gmail.com>
2022-02-14 14:17:32 -0500

    primera app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Hola from "./components/hola";
+
+
+
+function App() {
+  return (
+    
+    <div>
+      <h1>hola mundo react</h1>
+      <Hola mensaje= "Este es un mensaje" />
+      <Hola mensaje= "Esmeraldas"/>
+      <Hola mensaje= "Ecuador" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hola.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hola(props) {
+    return (
+        <div>
+            <h1> PUCESE</h1>
+            <h2>{props.mensaje}</h2>
+            <h3>Mi primer componente React!!!</h3>
+        </div>
+    );
+    }
+
+    export default Hola;
+    
